3dd3915696aa0f0563d3d7624e297206db9db59e
Hurrieam <77335318+Hurrieam@users.noreply.github.com>
2021-01-13 00:26:04 +0800

    Add files via upload

---
+++ src/CaneX_alpha.js
@@ -0,0 +1,17 @@
+//CaneX_alpha
+
+function dprint(str) {
+    document.write(str);
+}
+
+function dprintln(str) {
+    document.writeln(str);
+}
+
+function cprint(str) {
+    console.log(str);
+}
+
+function cprintln(str) {
+    console.log(str + "\n");
+}
